c38cfcb685a1df5397028e610bc4b2980767ddc7
Richard Nienaber <rjnienaber@gmail.com>
2015-02-04 23:34:08 +0000

    extended run all shell script

--- code/run.sh
+++ code/run.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-rm -f results.csv
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 1 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 2 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 3 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 4 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 5 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 6 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 7 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 8 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 9 -d 600; done
-for f in memcache redis mysql postgresql; do echo; ruby benchmark.rb -b $f -t 10 -d 600; done
+#check all benchmarks work
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 10 -d 1 -o warmup.csv; done
+rm -f warmup.csv
+
+rm -f results_all.csv
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 1 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 2 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 3 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 4 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 5 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 6 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 7 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 8 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 9 -d 600 -o results_all.csv; done
+for f in memcache redis mongo mysql mysql_innodb mysql_active_record postgresql; do echo; ruby benchmark.rb -b $f -t 10 -d 600 -o results_all.csv; done
